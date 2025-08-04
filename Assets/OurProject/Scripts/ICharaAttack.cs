6eeba3960cf7ad887c6793976bc05c1d829da48d
s-hmd <s.hmd.jp@gmail.com>
2025-08-04 20:55:30 +0900

    スクリプタブルオブジェクト項目追加

---
+++ Assets/OurProject/Scripts/ICharaAttack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ICharaAttack
+{
+    public int HitCount();
+    public void HitCountDown();
+    public bool Attackimekanshi();
+
+}
--- Assets/OurProject/Scripts/charadata.cs
+++ Assets/OurProject/Scripts/charadata.cs
@@ -14,4 +14,6 @@ public class charadata : ScriptableObject
     public int LV; //レベル
     public int GETEXP; //取得経験値
     public int GETGOLD; //取得できるお金
+    public int ShortAttackRange; //近接攻撃を行える距離
+    public float Enemytime; //攻撃などの行動を判断する処理を行う間隔
 }
